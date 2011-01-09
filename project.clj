9d78f9cd84362f0054bb78b7e15fe7a419fe40c7
George Jahad <george@runa.com>
2011-01-09 10:54:59 -0800

    moved to 0.3.1 debug-repl

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Clojure Debugging Toolkit"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.gjahad/debug-repl "0.3.0-SNAPSHOT"]])
+                 [org.clojars.gjahad/debug-repl "0.3.1"]])
