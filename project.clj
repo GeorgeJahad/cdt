44ea530dae1bdb3c189d9cc7510a290927b8041b
George Jahad <george@runa.com>
2011-01-09 10:56:33 -0800

    fix project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cdt "1.2.2"
+(defproject cdt "1.2.3"
   :description "Clojure Debugging Toolkit"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
