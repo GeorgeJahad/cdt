46d0c33086de818f917b9c113db42e3d62a122e3
James Norton <jamesnorton@gmail.com>
2016-10-03 18:30:38 -0400

    Added clansi dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject cdt  "1.2.6.3"
   :description "Clojure Debugging Toolkit"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clansi "1.0.0"]
                  [org.clojars.gjahad/debug-repl "0.3.1"]])
