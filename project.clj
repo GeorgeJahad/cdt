6ab3d1c3d160a9132c33848c2c114ca5112041f1
James Norton <james@element84.com>
2016-12-16 17:16:01 -0500

    Updated debug-repl dep

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Clojure Debugging Toolkit"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clansi "1.0.0"]
-                 [org.clojars.gjahad/debug-repl "0.3.1"]])
+                 [org.clojars.gjahad/debug-repl "0.3.3"]])
