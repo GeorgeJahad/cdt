a511e90b219aac611fa1ae1cdb0f8670297df259
James Norton <james@element84.com>
2017-01-17 09:48:13 -0500

    Updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jnorton/cdt  "1.2.6.3"
+(defproject org.clojars.jnorton/cdt  "1.2.6.4-SNAPSHOT"
   :description "Clojure Debugging Toolkit - changes to support VS Code Continuum"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clansi "1.0.0"]
